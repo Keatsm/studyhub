262831fcba544a5a6a19bb60fc52d8110a3d1f5b
keatsm <keatsm@zoho.com>
2024-05-11 02:09:09 +1000

    update

--- backend/src/main/java/backend/studyhub/StudyhubApplication.java
+++ backend/src/main/java/backend/studyhub/StudyhubApplication.java
@@ -1,4 +1,4 @@
-package studyhub;
+package backend.studyhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/backend/studyhub/StudyhubApplicationTests.java
+++ backend/src/test/java/backend/studyhub/StudyhubApplicationTests.java
@@ -1,4 +1,4 @@
-package studyhub;
+package backend.studyhub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
